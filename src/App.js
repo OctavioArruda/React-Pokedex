009829d053cd032c07381dbe0d879db59ce08b96
Octavio Arruda <octavio@maxiprod.com.br>
2020-07-18 18:19:29 -0300

    end of pokemon component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './components/layout/NavBar';
 import Dashboard from './components/layout/Dashboard';
-
+import Pokemon from './components/pokemon/Pokemon';
 
 class App extends Component {
   render() {
@@ -15,7 +15,10 @@ class App extends Component {
         <div className="App">
           <NavBar />
           <div className="container">
-            <Dashboard />
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />                  
+            </Switch>
           </div>
         </div>  
       </Router>  
